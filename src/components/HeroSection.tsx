300220811f6bff79a5ffdd83a1944a758ce4fc95
owaisrafiq05 <owaisrafiq05@gmail.com>
2025-08-29 17:17:07 +0500

    UI Minor Update 01

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -16,7 +16,7 @@ function HeroSection({
         backgroundPosition: 'center',
       }}
     >
-      <div className="absolute inset-0 bg-black/50" />
+      <div className="absolute inset-0 bg-black/10" />
       <div className="relative flex items-center justify-center h-full min-h-[300px] md:min-h-[400px] lg:min-h-[500px] px-4">
         <h1 className="text-3xl sm:text-4xl md:text-6xl font-extrabold tracking-tight text-white text-center mt-16 md:mt-0">
           {title}
